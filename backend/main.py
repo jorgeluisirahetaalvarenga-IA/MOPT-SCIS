1755d771f038e456811e8683c34cfd4bf6445a6c
Jorge Luis Iraheta <jorgeluisirahetaalvarenga@gmail.com>
2026-02-06 11:36:10 -0600

    Comentario sobre algo del main : host=0.0.0.0,  # nosec B104 - Aceptado para desarrollo por un warning que dio el security test

--- backend/main.py
+++ backend/main.py
@@ -681,7 +681,7 @@ if __name__ == "__main__":
     
     uvicorn.run(
         app,
-        host="0.0.0.0",
+        host="0.0.0.0",  # nosec B104 - Aceptado para desarrollo
         port=8000,
         reload=True
     )
